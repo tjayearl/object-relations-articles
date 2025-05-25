b1b49e432fde857b3586df155ea4886ca8d66fb4
tjayearl <iamtjayearl@gmail.com>
2025-05-25 21:37:15 +0300

    Initial commit

---
+++ scripts/setup_db.py
@@ -0,0 +1,12 @@
+from lib.db.connection import get_connection
+
+def setup_db():
+    with open("lib/db/schema.sql") as f:
+        schema = f.read()
+    conn = get_connection()
+    conn.executescript(schema)
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    setup_db()
